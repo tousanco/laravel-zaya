b84161eda896ea654b4f6343c0e53dc30dcd928c
Ali khosravi <alikhosravi175@gmail.com>
2023-09-02 19:53:54 +0330

    updated

--- src/Space.php
+++ src/Space.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static all(array $params = []): array
- * @method static create(array $params = []): array
+ * @method static create(array $params): array
  * @method static details(int $id): array
  * @method static update(int $id, array $params = []): array
  * @method static delete(int $id): array
